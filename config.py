ec9885936e56796a86602f4c054e33a8b39058e2
SAYAN <vladasdt2000@gmail.com>
2026-02-11 02:21:05 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    VOCAB_SIZE = 10000
+    HIDDEN_SIZE = 128
+    NUM_CLASSES = 3
+    LEARNING_RATE = 0.01
+    BATCH_SIZE = 32
+    EPOCHS = 100
+    MODEL_SAVE_PATH = "models/forcemini_v0.1.json"
+    VOCAB_SAVE_PATH = "vocab/forcemini_vocab.json"
+    MAX_SEQUENCE_LENGTH = 100
